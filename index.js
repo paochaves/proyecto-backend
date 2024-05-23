e05af439ccdd8b78b51b541ae2467d8ff322a6af
paola chaves <paochavess22@gmail.com>
2024-05-22 20:11:15 -0500

    eliminacion de node moduls y package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("works!");
+"use strict"
+const express = require("express");
+
+const servidor = express();
+
+servidor.get("/", (solicitud, respuesta)=>{ 
+    respuesta.json({ 
+        saludo: "Hola"
+    })
+});
+servidor.listen(3000);
+console.log("servidor corriendo en el puerto 3000");
+
+
