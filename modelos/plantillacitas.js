39a8fe4c8dc935f916b305283f3d0bcbd0ab8c4f
paola chaves <paochavess22@gmail.com>
2024-08-09 19:05:26 -0500

    creacion de rutas citas

---
+++ modelos/plantillacitas.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const citaSchema = new Schema({
+    hogarTemporal: {type: Boolean, required: true},
+    paseadorCanino: {type: Boolean, required: true},
+    voluntariadoDeLimpieza: {type: Boolean, required: true},
+    veterinaria: {type: Boolean, required: true},
+    donar: {type: Boolean, required: true},
+    fecha: {type: String, required: true},
+  });
+
+export default model("Cita", citaSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
---
+++ rutas/rutacitas.js
@@ -0,0 +1,18 @@
+import plantillacitas from "../modelos/plantillacitas.js";
+
+app.post('/submit', async (req, res) => {
+    const { hogarTemporal, paseadorCanino, voluntariadoDeLimpieza, veterinaria, donar, fecha } = req.body;
+  
+    const nuevaCita = new plantillacitas({
+      hogarTemporal: hogarTemporal === 'on',
+      paseadorCanino: paseadorCanino === 'on',
+      voluntariadoDeLimpieza: voluntariadoDeLimpieza === 'on',
+      veterinaria: veterinaria === 'on',
+      donar: donar === 'on',
+      fecha,
+    });
+  
+    await nuevaCita.save();
+    res.send('Su cita fue agendada con éxito');
+  });
+  
